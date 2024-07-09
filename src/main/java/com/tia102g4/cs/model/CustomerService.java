e485c6a7e95b2e8e674a4cea5431f17827eb1a82
Cheng Yi Lin <charlielin05060506@gmail.com>
2024-07-09 18:41:13 +0800

    first commit

---
+++ src/main/java/com/tia102g4/cs/model/CustomerService.java
@@ -0,0 +1,18 @@
+package com.tia102g4.cs.model;
+
+public class CustomerService {
+	private Integer empno;
+	private String ename;
+	public Integer getEmpno() {
+		return empno;
+	}
+	public void setEmpno(Integer empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+}
