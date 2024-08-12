b32d1b540c3414086ebe5d6399018bd8a7fdf58b
ficus <j307582@gmail.com>
2024-08-12 17:55:06 +0800

    新增RestaurantDTO

---
+++ src/main/java/com/tia102g4/rest/to/RestaurantDTO.java
@@ -0,0 +1,12 @@
+package com.tia102g4.rest.to;
+
+public class RestaurantDTO {
+	private String restaurantId;
+    private String itemName;
+    private String itemDescription;
+
+    // Getters
+    public String getRestaurantId() { return restaurantId; }
+    public String getItemName() { return itemName; }
+    public String getItemDescription() { return itemDescription; }
+}
